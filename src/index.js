134bcc81b006f74a915151c7dee3160521743e8e
Andrea Bustos <andreastefbustos@gmail.com>
2023-06-06 13:35:12 -0400

    Modificacion getElementsByName()/ utilizando querySelector

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
-const userInput = document.getElementsByName('user-input');
+const userInput = document.querySelector('textarea[name = "user-input"]');
 userInput.addEventListener('keyup', textAnalyzer);
 
 const resetButton = document.getElementById('reset-button');
 resetButton.addEventListener('click', reset);
 
 function textAnalyzer(){
-  const textArea = document.getElementById('user-input').value;
+  const textArea = document.querySelector('textarea[name = "user-input"]').value;
   const wordCount = analyzer.getWordCount(textArea);
   const charaterCount = analyzer.getCharacterCount(textArea);
   const characterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(textArea);
@@ -24,7 +24,7 @@ function textAnalyzer(){
 }
 
 function reset(){
-  document.getElementById('user-input').value = " ";
+  document.querySelector('textarea[name = "user-input"]').value = " ";
   document.querySelector('#word-count').innerHTML = "0";
   document.querySelector('#charater-count').innerHTML = "0";
   document.querySelector('#character-count-excluding-spaces').innerHTML = "0";
